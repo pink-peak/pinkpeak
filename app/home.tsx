da4ca9f48604a24bbb263e05f71fe0f9481fbf9f
pink-peak <official@pinkpeak.art>
2025-04-15 19:10:51 +0900

    Update home.tsx

--- app/home.tsx
+++ app/home.tsx
@@ -9,14 +9,8 @@ const navigation = [
 
 export default function Home() {
   return (
-    <div
-      className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-cover bg-center"
-      style={{ backgroundImage: "url('/logo.png')" }}
-    >
-      {/* 어두운 오버레이가 필요하면 추가 */}
-      {/* <div className="absolute inset-0 bg-black/40 z-0" /> */}
-
-      <nav className="my-16 animate-fade-in z-10">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -29,20 +23,30 @@ export default function Home() {
           ))}
         </ul>
       </nav>
-
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 z-10" />
-
-      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
-
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         PINK PEAK
       </h1>
 
-      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0 z-10" />
-
-      <div className="my-16 text-center animate-fade-in z-10">
-        <h2 className="text-xl text-zinc-400 font-semibold">SOUND STORY STAY</h2>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-xl text-zinc-400 font-semibold">
+              SOUND STORY STAY
+{/*           I'm building{" "}
+          <Link
+            target="_blank"
+            href="https://unkey.dev"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            unkey.dev
+          </Link> to solve API authentication and authorization for developers. */}
+        </h2>
       </div>
     </div>
   );
+
 }

17242bf8ad70c3927cd9815ac5c1669d4afbd53c
pink-peak <official@pinkpeak.art>
2025-04-15 19:41:35 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import React from 'react';
@@ -6,17 +7,22 @@ import React from 'react';
 export default function Intro() {
   const router = useRouter();
 
+  const handleClick = () => {
+    router.push('/main');
+  };
+
   return (
     <div
-      onClick={() => router.push('/main')}
+      onClick={handleClick}
       className="w-screen h-screen flex items-center justify-center bg-black cursor-pointer"
     >
       <Image
-        src="/logo.png" // 또는 너가 넣은 이미지 이름
+        src="/logo.png"
         alt="Intro Cover"
         width={800}
         height={800}
         priority
+        className="hover:scale-105 transition-transform duration-500"
       />
     </div>
   );

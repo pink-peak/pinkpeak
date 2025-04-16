b12ae3d98b21515d859196e857de5509fb76f02b
pink-peak <official@pinkpeak.art>
2025-04-16 12:26:41 +0900

    Create fonts.ts

---
+++ fonts.ts
@@ -0,0 +1,19 @@
+// app/fonts.ts
+import localFont from 'next/font/local';
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: '../public/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--font-pretendard',
+});
